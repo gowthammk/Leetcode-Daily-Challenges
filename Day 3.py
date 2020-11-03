f4d1dbfea9952e3229adfedc8786ec740e635068
Gowtham <mkgowtham7@gmail.com>
2020-11-03 18:03:53 +0000

    First Commit

---
+++ Day 3.py
@@ -0,0 +1,13 @@
+# Given a string s, the power of the string is the
+# maximum length of a non-empty substring that contains
+# only one unique character.
+
+# Return the power of the string.
+
+# Input: s = "leetcode"
+# Output: 2
+# Explanation: The substring "ee" is of length 2 with the character 'e' only.
+
+class Solution:
+    def maxPower(self, s):
+        pass
