9f1c072e46fc6658106a8b2bd145c9babcc71d5e
Gowtham <mkgowtham7@gmail.com>
2020-11-03 18:22:28 +0000

    Final Commit

--- Day 3.py
+++ Day 3.py
@@ -4,10 +4,48 @@
 
 # Return the power of the string.
 
+# Example 1:
+
 # Input: s = "leetcode"
 # Output: 2
 # Explanation: The substring "ee" is of length 2 with the character 'e' only.
 
+#Example 2:
+
+#Input: s = "abbcccddddeeeeedcba"
+#Output: 5
+#Explanation: The substring "eeeee" is of length 5 with the character 'e' only.
+#Example 3:
+
+#Input: s = "triplepillooooow"
+#Output: 5
+#Example 4:
+
+#Input: s = "hooraaaaaaaaaaay"
+#Output: 11
+#Example 5:
+
+#Input: s = "tourist"
+#Output: 1
+
 class Solution:
     def maxPower(self, s):
-        pass
+        count = 1
+        maximum = 1
+        for i in range(0, len(s) - 1):
+            if (s[i] == s[i + 1]):
+                count += 1
+                maximum = max(maximum, count)
+            elif (s[i] != s[i + 1]):
+                count = 1
+        return maximum
+
+
+
+Solution.maxPower("", "leetcode")
+Solution.maxPower("", "abbcccddddeeeeedcba")
+Solution.maxPower("", "triplepillooooow")
+Solution.maxPower("", "hooraaaaaaaaaaay")
+Solution.maxPower("", "tourist")
+Solution.maxPower("", "cc")
+
