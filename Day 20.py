a540f895ca1c3ea18a6704c885f7946b90862c51
Gowtham <mkgowtham7@gmail.com>
2020-11-21 16:31:51 +0000

    Binary search update

--- Day 20.py
+++ Day 20.py
@@ -20,9 +20,35 @@
 
 #
 
+"""class Solution:
+    def search(self, nums, target):
+        def helper(mid, target):
+            for i in range(len(mid)):
+                if nums[i] == target:
+                    return True
+            return False
+        nums = sorted(nums)
+        mid = len(nums)//2
+        print(nums, nums[mid])
+        if target >= nums[mid]:
+            return(helper(nums[mid-1:], target))
+        else:
+            return(helper(nums[:mid], target))"""
+
 class Solution:
-    def search(self, nums: List[int], target: int) -> bool:
-        for i in range(len(nums)):
-            if nums[i] == target:
-                return True
-        return False
+    def search(self, nums, target):
+        def helper(mid, target):
+            for i in range(len(mid)):
+                    if mid[i] == target:
+                        return True
+            return False
+        if len(nums) == 0:
+                return
+        nums = sorted(nums)
+        mid = len(nums)//2
+        if target >= nums[mid]:
+            return(helper(nums[mid:], target))
+        else:
+            return(helper(nums[:mid], target))
+
+print(Solution.search("",[2,5,6,0,0,1], 2))
